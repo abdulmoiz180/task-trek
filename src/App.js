781cd547736642e0a62f765cc31ec16652af8216
Fida-khaplu <fidakhaplu@gmail.com>
2023-09-21 13:04:17 +0500

    Forget Password Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import './app.css'
+import './App.css'
+import ForgetPassword from './pages/ForgetPassword'
 const App = () => {
   return (
     <main className='App'>
+      
       <h1>Welcome to Task Trek</h1>
       <p>A <span>Project Management System</span>, Where you can perform <span>CRUD </span>operations  on tasks.</p>
     </main>
