d1b2df42ff09deed9bae5824bd22e524a76e06a8
Fida-khaplu <fidakhaplu@gmail.com>
2023-09-25 15:21:39 +0500

    changes in app js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ForgetPassword from './pages/ForgetPassword'
 const App = () => {
   return (
     <main className='App'>
-      
+      <ForgetPassword/>
       <h1>Welcome to Task Trek</h1>
       <p>A <span>Project Management System</span>, Where you can perform <span>CRUD </span>operations  on tasks.</p>
     </main>
