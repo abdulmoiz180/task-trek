5c7e0d3c4b3896008692b0e3f5e74c1204999f5f
abdulmoiz <86162711+abdulmoiz180@users.noreply.github.com>
2024-02-16 01:12:37 +0500

    Removed firebase config keys

--- src/utils/constants/Firebase.js
+++ src/utils/constants/Firebase.js
@@ -6,13 +6,7 @@ import {
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAk4dY00qmAKeQ8Ndx8ySZEIiwguzIn7I",
-  authDomain: "task-trek-c6b75.firebaseapp.com",
-  databaseURL: "https://task-trek-c6b75-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "task-trek-c6b75",
-  storageBucket: "task-trek-c6b75.appspot.com",
-  messagingSenderId: "1033846365318",
-  appId: "1:1033846365318:web:ade2c5ae2e80050b204507"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
